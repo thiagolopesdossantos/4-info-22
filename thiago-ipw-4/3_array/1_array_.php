8792319f5aa2ccf926b02ac94a66c6f8c8eee6cd
thiago dos santos <108496753+thiagolopesdossantos@users.noreply.github.com>
2022-06-30 20:47:39 -0300

    Add files via upload

---
+++ thiago-ipw-4/3_array/1_array_.php
@@ -0,0 +1,34 @@
+<?php
+    $cores1 = array('vermelho','azul','verde','amarelo');
+	var_dump($cores1);
+	echo '<br>';
+	echo $cores1[0];
+	echo '<br>';
+	echo $cores1[1];
+	echo '<br>';
+	echo $cores1[2];
+	echo '<br>';
+	echo $cores1[3];
+	echo '<hr>';
+	
+	$frutas = array(0=>'jabuticaba',1=>'acerola', 2=>'laranja');
+	var_dump($frutas);
+	echo '<br>';
+	echo $frutas[0];
+	echo '<br>';
+	echo $frutas[1];
+	echo '<br>';
+	echo $frutas[2];
+	echo '<hr>';
+	
+	
+	$carros = [0=>'nissangtrr35',1=>'lamborghiniaventador',2=>'ferrari458pista'];
+	var_dump($carros);
+	echo '<br>';
+	echo $carros[0];
+	echo '<br>';
+	echo $carros[1];
+	echo '<br>';
+	echo $carros[2];
+	echo '<hr>';
+?>
